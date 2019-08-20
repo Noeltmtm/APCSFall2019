e0d12f11256e16ec6d5a0c21d5cd192562707b25
APCS3rd <APCS3rd@MHS-LT9-13>
2019-08-20 10:13:04 -0700

    Complete Hello World Project.

---
+++ Hello World Project/src/MyFirstProgram.java
@@ -0,0 +1,11 @@
+//Noel Lkhagvatsogt 8/16/19 Period 3
+//This program prints 'Hello World' to the console
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
